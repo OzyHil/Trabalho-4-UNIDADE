8fd45049b1b9376d36548f7579b34fd68dacb4d6
OzyHil <117992907+OzyHil@users.noreply.github.com>
2022-12-20 19:30:52 -0300

    Add files via upload

--- Escola.java
+++ Escola.java
@@ -1,50 +1,138 @@
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Escola{
-    public HashMap<Integer,Docente> docentes;
-    public HashMap<Integer,Disciplina> disciplinas;
-    public HashMap<Integer,Aluno> alunos;
-    public HashMap<Integer,Turma> turmas;;
-    private GerenteDeArquivo ga;
+    public LinkedHashMap<Integer,Docente> docentes = new LinkedHashMap<Integer,Docente>();
+    public LinkedHashMap<Integer,Disciplina> disciplinas = new LinkedHashMap<Integer,Disciplina>();
+    public LinkedHashMap<Integer,Aluno> alunos = new LinkedHashMap<Integer,Aluno>();
+    public LinkedHashMap<Integer,Turma> turmas = new LinkedHashMap<Integer,Turma>();
+    private int maiorAluno,maiorDocente,maiorDisciplina,maiorTurma;
+    private GerenteDeArquivo ga = new GerenteDeArquivo();
 
     //Metódos
-    public void adicionarAluno(){}
+    //TODO fazer os métodos de adição e adição darem throws qnd acessar um item q nao existem
+    public Aluno adicionarAluno(String nome){
+        Aluno aluno = new Aluno(nome,maiorAluno);
+        alunos.put(maiorAluno, aluno);
+        maiorAluno++;
+        return aluno;
+    }
 
-    public void removerAluno(){}
+    public void removerAluno(int codigo) throws NoSuchKeyException{
+        if (!alunos.containsKey(codigo))
+            throw new NoSuchKeyException();
+        else
+            alunos.remove(codigo);
+    }
 
-    public void adicionarDocente(){}
+    public Docente adicionarDocente(String nome){
+        Docente docente = new Docente(nome, maiorDocente);
+        docentes.put(maiorDocente, docente);
+        maiorDocente++;
+        return docente;
+    }
 
-    public void removerDocente(){}
+    public void removerDocente(int codigo) throws NoSuchKeyException{
+        if (!docentes.containsKey(codigo))
+            throw new NoSuchKeyException();
+        else
+            docentes.remove(codigo);
+    }
 
-    public void adicionarDisciplina(){}
+    public Disciplina adicionarDisciplina(String nome){
+        Disciplina disc =  new Disciplina(nome, maiorDisciplina);
+        disciplinas.put(maiorDisciplina, disc);
+        maiorDisciplina++;
+        return disc;
+    }
 
-    public void removerDisciplina(){}
+    public void removerDisciplina(int codigo) throws NoSuchKeyException{
+        if (!disciplinas.containsKey(codigo))
+            throw new NoSuchKeyException();
+        else
+            disciplinas.remove(codigo);
+    }
 
-    public void adicionarTurma(){}
+    public Turma adicionarTurma(String nome){
+        Turma turma = new Turma(nome,maiorTurma);
+        turmas.put(maiorTurma, turma);
+        maiorTurma++;
+        return turma;
+    }
 
-    public void removerTurma(){}
+    public void removerTurma(int codigo) throws NoSuchKeyException{
+        if (!turmas.containsKey(codigo))
+            throw new NoSuchKeyException();
+        else
+            turmas.remove(codigo);
+    }
+
+    public void editarAluno(){};
+    public void editarTurma(){};
+    public void editarDisciplina(){};
+    public void editarDocente(){};
 
     //Os métodos abaixo estão retornando null enquanto não são devidamente implementados, pra não dar erro no java
 
-    public String mapearDocentes(){return null;} //Retorna o nome dos docentes e seus códigos.
+    public String mapearDocentes(){
+        String toReturn = "";
+        for (Map.Entry<Integer, Docente> pair:docentes.entrySet()){
+            toReturn = toReturn + pair.getKey() + " - " +pair.getValue().getNome() + "\n"; 
+        } 
+        return toReturn;
+    } //Retorna o nome dos docentes e seus códigos.
 
-    public String mapearDisciplinas(){return null;}//Retorna o nome das disciplinas e seus códigos.
+    public String mapearDisciplinas(){        
+        String toReturn = "";
+        for (Map.Entry<Integer, Disciplina> pair:disciplinas.entrySet()){
+            toReturn = toReturn + pair.getKey() + " - " +pair.getValue().getNome() + "\n"; 
+        } 
+        return toReturn;
+    }//Retorna o nome das disciplinas e seus códigos.
 
-    public String mapearAlunos(){return null;}//Retorna o nome dos alunos e seus códigos.
+    public String mapearAlunos(){
+        String toReturn = "";
+        for (Map.Entry<Integer, Aluno> pair:alunos.entrySet()){
+            toReturn = toReturn + pair.getKey() + " - " +pair.getValue().getNome() + "\n"; 
+        } 
+        return toReturn;
+    }//Retorna o nome dos alunos e seus códigos.
 
-    public String mapearTurmas(){return null;}//Retorna o nome das turmas e seus códigos.
-
-    public void editarDocente(){}//Permite mudar informações de um docente, incluindo disciplinas.
-
-    public void editarDisciplina(){}//Permite mudar informações de uma disciplina, incluindo docentes, alunos e notas. 
-    //Não deixar disciplina ficar sem docente. Não deixar adicionar aluno de ano errado ser cadastrado.
-
-    public void editarAluno(){}//Permite mudar informações de um aluno, incluindo turmas.
-
-    public void editarTurma(){}//Permite mudar informações de um docente, incluindo alunos.
+    public String mapearTurmas(){
+        String toReturn = "";
+        for (Map.Entry<Integer, Turma> pair:turmas.entrySet()){
+            toReturn = toReturn + pair.getKey() + " - " +pair.getValue().getNome() + "\n"; 
+        } 
+        return toReturn;
+    }//Retorna o nome das turmas e seus códigos.
 
     public void gerarRelatórios(int tipo){}//Gerar relatórios com o GeradorDeRelatorio e passar pro arquivo com o GerenteDeArquivos.
 
-    public void carregar(){}//Inicializar dados a partir de informações de arquivos.
+    public void carregar(){
+        Aluno a;
+        for (String base : ga.carregarAlunos()) {
+            a = new Aluno(base);
+            alunos.put(a.getCodigo(), a); 
+        }
+        Docente doc;
+        for (String base : ga.carregarDocentes()) {
+            doc = new Docente(base);
+            docentes.put(doc.getCodigo(), doc); 
+        }
+        Turma t;
+        for (String base : ga.carregarTurmas()) {
+            t = new Turma(base,this);
+            turmas.put(t.getCodigo(), t); 
+        }
+        Disciplina dic;
+        for (String base : ga.carregarDisciplinas()) {
+            dic = new Disciplina(base,this);
+            disciplinas.put(dic.getCodigo(), dic); 
+        }
+    }
+
+    public void salvar(){
+        ga.escrever(this);
+    }
     
-}
+}   
